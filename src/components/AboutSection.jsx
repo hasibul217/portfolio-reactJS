151f15b989e11f4be531b63a90877e32569f182b
hasibul217 <shantossnic@gmail.com>
2025-07-11 19:46:08 +0600

    cv added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -48,7 +48,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href=""
+                href="/public/Hasibul_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
